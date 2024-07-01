a4574385d0077e46ea4ad878fa096862e8570a69
less <fabien@bonustrack.co>
2024-07-01 10:55:58 +0700

    chore: remove totalDelegates

--- src/utils.ts
+++ src/utils.ts
@@ -1,33 +1,22 @@
-import { validateAndParseAddress } from 'starknet';
 import { Delegate, Governance } from '../.checkpoint/models';
 
 export const DECIMALS = 18;
 
 export const BIGINT_ZERO = BigInt(0);
 
-export const ZERO_ADDRESS = validateAndParseAddress('0x0');
-
-export async function getDelegate(id: string, governanceId: string) {
+export async function getDelegate(id: string, governanceId: string): Promise<Delegate> {
   let delegate = await Delegate.loadEntity(`${governanceId}/${id}`);
 
   if (!delegate) {
     delegate = new Delegate(`${governanceId}/${id}`);
     delegate.governance = governanceId;
     delegate.user = id;
-
-    if (id != ZERO_ADDRESS) {
-      const governance = await getGovernance(governanceId);
-
-      governance.totalDelegates += 1;
-
-      await governance.save();
-    }
   }
 
   return delegate;
 }
 
-export async function getGovernance(id: string) {
+export async function getGovernance(id: string): Promise<Governance> {
   let governance = await Governance.loadEntity(id);
 
   if (!governance) {
